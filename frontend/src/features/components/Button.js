d6137439abe54def78fdaf7465de7af33f3226d1
vutien <tienloveu123@gmail.com>
2022-04-26 12:13:20 +0700

    tienPush24

--- frontend/src/features/components/Button.js
+++ frontend/src/features/components/Button.js
@@ -8,14 +8,14 @@ const Button = ({ show, showBtn }) => {
           className="btnfloat rounded-full p-3 m-3 text-white font-extrabold shadow-xl animate-pulse  bg-gradient-to-r from-green-400 to-blue-500"
           onClick={() => showBtn()}
         >
-          <i className="far fa-plus"></i>
+          <i className="fas fa-plus-circle" aria-hidden="true" style={{color: "blue",fontSize:"27px"}}></i>
         </button>
       ) : (
         <button
           className="btnfloat rounded-full p-3 m-3 text-white font-semibold shadow-xl  bg-gradient-to-r from-green-400 to-blue-500"
           onClick={() => showBtn(!show)}
         >
-          <i className="far fa-minus"></i>
+         <i className="fas fa-plus-circle" aria-hidden="true"></i>
         </button>
       )}
     </>
